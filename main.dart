692e81b299b19ce4cb23991e0678ea81cc062722
arsalanfurqan <arsalan.furqan101@gmail.com>
2024-09-08 00:03:22 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,52 @@
+void main(){
+ names = ["Alice", "Bob", "Charlie", "David"]
+
+# Print all names
+for name in names:
+    print(name)
+ days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+# Print Last Sunday
+    print("Sunday")
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
